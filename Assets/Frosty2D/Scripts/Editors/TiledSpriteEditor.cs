19e5253090b6c87d1cbb05cdabe2a3a6071a2b35
Conrado Adevany Clarke <conrado.clarke@cnova.com>
2016-05-05 16:53:31 -0300

    Initial Commit (Tiled Sprites, Unlit Sprite Shader w/ Saturation,Hue, Luminance, Colorization, Color Invertion, and UV scrolling by time)

---
+++ Assets/Frosty2D/Scripts/Editors/TiledSpriteEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(TiledSprite))]
+[CanEditMultipleObjects]
+public class TiledSpriteEditor : Editor{
+    void OnEnable()
+    {
+        var castedTarget = (target as TiledSprite);
+        var meshRenderer = castedTarget.GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+        {
+            meshRenderer.hideFlags = HideFlags.HideInInspector;
+        }
+
+        var meshFilter = castedTarget.GetComponent<MeshFilter>();
+        if (meshFilter != null)
+        {
+            meshFilter.hideFlags = HideFlags.HideInInspector;
+        }
+        var preview = AssetPreview.GetAssetPreview(castedTarget);
+        preview = castedTarget.sprite.texture;
+    }
+}
---
+++ Assets/testmovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testmovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localPosition += Vector3.right * 10;
+	}
+}
